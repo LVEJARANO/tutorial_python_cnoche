a06a3dc61da4e564cfc45d55d297691a11f5eae2
Luis Vejarano <luis.vejarano1.0@gmail.com>
2023-03-09 00:53:04 -0500

    Se agrega los tipos de datos

---
+++ repasoPython/main.py
@@ -0,0 +1,25 @@
+# Tipos de datos
+# Numeros enteros
+
+numero1: int = 78
+numero2 = 45
+print(numero1)
+print(numero2)
+
+# Numeros reales
+
+numero3: float = 4.9
+numero4 = 5.0
+print(numero3)
+print(numero4)
+
+# Booleanos toman dos valores Verdadero o Falso
+
+esColombiano: bool = True
+esArgentino = False
+print(esColombiano)
+print(esArgentino)
+
+# Caracter y Cadena de caracteres
+mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
+print(mensaje)
